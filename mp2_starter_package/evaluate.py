c91baa989731ccdc2fbffe2f24b87d9aa8b9d437
zoeyz5 <xiaozhou@umail.ucsb.edu>
2021-05-23 14:34:15 +0800

    Add files via upload

---
+++ mp2_starter_package/evaluate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys, os, os.path
+import json
+
+if __name__ == "__main__":
+
+    # Load student code
+    from mp2 import run_train_test
+
+    train_data = json.load(open('train.json'))
+    dev_data = json.load(open('dev.json'))
+
+    prediction = run_train_test(train_data['data'], train_data['label'], dev_data['data'])
+
+    accuracy = len([i for i in range(len(prediction)) if prediction[i] == dev_data['label'][i]]) / float(len(prediction))
+    '''
+    TP = len([i for i in range(len(prediction)) if prediction[i] == dev_data['label'][i] and dev_data['label'][i] > 0])
+    FP = len([i for i in range(len(prediction)) if prediction[i] == dev_data['label'][i] and dev_data['label'][i] > 0])
+    all_pos = len([_ for _ in dev_data['label'] if _ > 0])
+    if float(TP+FP) > 0:
+        precision = TP / float(TP+FP)
+    else:
+        precision = 0
+    recall = TP / float(all_pos)
+    if precision + recall > 0:
+        f1 = 2*precision*recall/(precision+recall)
+    else:
+        f1 = 0
+    print(f1)
+    '''
+    print(accuracy)
---
+++ mp2_starter_package/mp2.py
@@ -0,0 +1,140 @@
+# Starter code for CS 165B HW3
+# reference to: https://machinelearningmastery.com/implement-decision-tree-algorithm-scratch-python/
+import numpy as np
+import json
+
+f = open('train.json')
+train = json.load(f)
+abalone = np.array(train["data"])
+label = np.array(train["label"])
+abalone = np.column_stack((abalone, label))
+abalone = abalone[0:50]
+
+
+f = open('dev.json')
+test = json.load(f)
+abalone_t = np.array(test["data"])
+abalone_t = abalone_t[0:50]
+
+def run_train_test(training_data, training_labels, testing_data):
+    """
+    Implement the training and testing procedure here. You are permitted
+    to use additional functions but DO NOT change this function definition.
+
+    Inputs:
+        training_data: List[List[float]]
+        training_label: List[int]
+        testing_data: List[List[float]]
+
+    Output:
+        testing_prediction: List[int]
+    Example:
+    return [1]*len(testing_data)
+    """
+    training_data = np.column_stack((training_data, training_labels))
+    return decision_tree(training_data,testing_data,8,10)
+
+    #TODO implement the decision tree and return the prediction
+
+# Split a dataset based on an attribute and an attribute value
+def dataset_split(index, value, dataset):
+	left = list()
+	right = list()
+	for row in dataset:
+		if row[index] < value:
+			left.append(row)
+		else:
+			right.append(row)
+	return left, right
+
+# Calculate the Gini index for a split dataset
+def gini_index(groups, classes):
+	# count all samples at split point
+	n_instances = float(sum([len(group) for group in groups]))
+	# sum weighted Gini index for each group
+	gini = 0.0
+	for group in groups:
+		size = float(len(group))
+		# avoid divide by zero
+		if size == 0:
+			continue
+		score = 0.0
+		# score the group based on the score for each class
+		for class_val in classes:
+			p = [row[-1] for row in group].count(class_val) / size
+			score += p * p
+		# weight the group score by its relative size
+		gini += (1.0 - score) * (size / n_instances)
+	return gini
+
+# Select the best split point for a dataset
+def best_split(dataset):
+	temp = np.array(dataset)
+	label = temp[:,-1]
+	label = np.unique(label)
+	b_index, b_value, b_score, b_groups = 999, 999, 999, None
+	for index in range(len(dataset[0])-1):
+		for row in dataset:
+			groups = dataset_split(index, row[index], dataset)
+			gini = gini_index(groups, label)
+			if gini < b_score:
+				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+	return {'index':b_index, 'value':b_value, 'groups':b_groups}
+
+# Create a terminal node value
+def to_terminal(group):
+	outcomes = [row[-1] for row in group]
+	return max(set(outcomes), key=outcomes.count)
+
+# Create child splits for a node or make terminal
+def split(node, max_depth, min_size, depth):
+	left, right = node['groups']
+	del(node['groups'])
+	# check for a no split
+	if not left or not right:
+		node['left'] = node['right'] = to_terminal(left + right)
+		return
+	# check for max depth
+	if depth >= max_depth:
+		node['left'], node['right'] = to_terminal(left), to_terminal(right)
+		return
+	# process left child
+	if len(left) <= min_size:
+		node['left'] = to_terminal(left)
+	else:
+		node['left'] = best_split(left)
+		split(node['left'], max_depth, min_size, depth+1)
+	# process right child
+	if len(right) <= min_size:
+		node['right'] = to_terminal(right)
+	else:
+		node['right'] = best_split(right)
+		split(node['right'], max_depth, min_size, depth+1)
+
+# Build a decision tree
+def build_tree(train, max_depth, min_size):
+	root = best_split(train)
+	split(root, max_depth, min_size, 1)
+	return root
+
+# Make a prediction with a decision tree
+def predict(node, row):
+	if row[node['index']] < node['value']:
+		if isinstance(node['left'], dict):
+			return predict(node['left'], row)
+		else:
+			return node['left']
+	else:
+		if isinstance(node['right'], dict):
+			return predict(node['right'], row)
+		else:
+			return node['right']
+
+# Classification and Regression Tree Algorithm
+def decision_tree(train, test, max_depth, min_size):
+	tree = build_tree(train, max_depth, min_size)
+	predictions = list()
+	for row in test:
+		prediction = predict(tree, row)
+		predictions.append(prediction)
+	return(predictions)
